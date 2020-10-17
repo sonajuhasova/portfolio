da06fea47845b416ba30fbb95c525733c3ca0f50
Sona <sona.juhasova@gmail.com>
2020-10-17 20:18:59 +0200

    test scroll eff

---
+++ fullpage-scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      navigation: true,
+      navigationTooltips: ['intro', 'user-journey','user-journey2','colour-coding','cc-mockups','api',],
+      showActiveTooltip: true,
+      slidesNavigation: true,
+      controlArrows:false,
+    });
+});

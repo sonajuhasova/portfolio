bba2ab521c38f557b8bf9b05a7d5458a70cb0ce1
Sona <sona.juhasova@gmail.com>
2020-10-17 20:34:14 +0200

    dnjk

--- fullpage-scroll.js
+++ fullpage-scroll.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
       navigation: true,
+      navigationPosition: 'right',
       navigationTooltips: ['intro', 'user-journey','user-journey2','colour-coding','cc-mockups','api',],
       showActiveTooltip: true,
       slidesNavigation: true,
+        slidesNavPosition: 'bottom',
       controlArrows:false,
     });
 });
